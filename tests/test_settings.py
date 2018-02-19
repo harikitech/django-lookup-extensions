2a693f881fc4ce0faf18cd3b1e17fdebc879cee8
makoto tsuyuki <tsuyuki@uncovertruth.co.jp>
2018-02-19 16:04:33 +0900

    add database vendor type mysql and postgresql

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,4 +1,6 @@
 """Django settings for tests."""
+import os
+
 
 SECRET_KEY = 'fake-key'
 INSTALLED_APPS = [
@@ -8,13 +10,38 @@ INSTALLED_APPS = [
     'tests',
 ]
 
-
-DATABASES = {
-    'default': {
-        'NAME': 'default',
-        'ENGINE': 'django.db.backends.sqlite3',
-        'TEST': {
-            'NAME': 'file:default?mode=memory&cache=shared',
+if os.environ.get('TEST_DB_VENDOR', None) == 'mysql':
+    import pymysql
+    pymysql.install_as_MySQLdb()
+    DATABASES = {
+        'default': {
+            'NAME': 'default',
+            'ENGINE': 'django.db.backends.mysql',
+            'USER': 'root',
+            'PASSWORD': '',
+            'TEST': {
+                'NAME': 'test_database',
+            }
         },
-    },
-}
+    }
+elif os.environ.get('TEST_DB_VENDOR', None) == 'postgresql':
+    DATABASES = {
+        'default': {
+            'NAME': 'default',
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'USER': 'postgres',
+            'TEST': {
+                'NAME': 'test_database',
+            }
+        },
+    }
+else:
+    DATABASES = {
+        'default': {
+            'NAME': 'default',
+            'ENGINE': 'django.db.backends.sqlite3',
+            'TEST': {
+                'NAME': 'file:default?mode=memory&cache=shared',
+            }
+        },
+    }
