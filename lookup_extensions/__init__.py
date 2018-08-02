dfcaabb929e7384fdd906ac29c13b86961aaf224
Kosei Kitahara <surgo.jp@gmail.com>
2018-08-02 20:03:24 +0900

    Bump version to 0.1.1

--- lookup_extensions/__init__.py
+++ lookup_extensions/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """UNCOVER TRUTH Inc."""
 __email__ = 'develop@uncovertruth.co.jp'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 default_app_config = 'lookup_extensions.apps.LookupExtensionsConfig'
