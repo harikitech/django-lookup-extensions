368e0b06df579b6bec0394fad62d55e093641a93
Kosei Kitahara <surgo.jp@gmail.com>
2018-08-02 19:54:32 +0900

    QuerySet class as a mixin

--- lookup_extensions/sql.py
+++ lookup_extensions/sql.py
@@ -4,7 +4,7 @@ from django.db.models.constants import LOOKUP_SEP
 from django.db.models.sql import Query as DjangoQuery
 
 
-class Query(DjangoQuery):
+class ExtendedQueryMixin(object)
     SUBQUERY_LOOKUPS = (
         'exists',
         'neexists',
@@ -14,18 +14,22 @@ class Query(DjangoQuery):
         if lookups and lookups[-1] in self.SUBQUERY_LOOKUPS:
             if not isinstance(rhs, Exists):
                 raise FieldError("Value is not Subquery instance.")
-            return super(Query, self).build_lookup(['exact'], rhs, not lookups[-1].startswith('ne'))
-        return super(Query, self).build_lookup(lookups, lhs, rhs)
+            return super(ExtendedQueryMixin, self).build_lookup(['exact'], rhs, not lookups[-1].startswith('ne'))
+        return super(ExtendedQueryMixin, self).build_lookup(lookups, lhs, rhs)
 
     def prepare_lookup_value(self, value, lookups, can_reuse, allow_joins=True):
         if lookups and lookups[-1] in self.SUBQUERY_LOOKUPS:
             value = value.resolve_expression(self, allow_joins=True, reuse=None, summarize=False)
             return value, lookups, []
-        return super(Query, self).prepare_lookup_value(
+        return super(ExtendedQueryMixin, self).prepare_lookup_value(
             value, lookups, can_reuse, allow_joins=allow_joins)
 
     def solve_lookup_type(self, lookup):
         lookup_splitted = lookup.split(LOOKUP_SEP)
         if lookup_splitted and lookup_splitted[-1] in self.SUBQUERY_LOOKUPS:
             return lookup_splitted, (), True  # No JOIN
-        return super(Query, self).solve_lookup_type(lookup)
+        return super(ExtendedQueryMixin, self).solve_lookup_type(lookup)
+
+
+class Query(ExtendedQueryMixin, DjangoQuery):
+    pass
